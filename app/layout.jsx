cb87ee3f5fa638a9548495c8b17df28103357b36
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-04-24 18:23:14 +0600

    Font configuration added

---
+++ app/layout.jsx
@@ -0,0 +1,42 @@
+import { Inter, Poppins, Urbanist } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+const urbanist = Urbanist({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-urbanist",
+});
+
+const berlinSans = localFont({
+  src: [
+    {
+      path: "../public/fonts/BRLNSDB.woff",
+      style: "normal",
+    },
+  ],
+  variable: "--font-berlin-sans",
+});
+
+export const metadata = {
+  title: "Kainu",
+  description: "Streaming vidio games online on live Kainu.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${berlinSans.variable} ${urbanist.variable} ${poppins.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-7xl font-urbanist">Mahedi Hasan</h1>
+    </main>
+  );
+}

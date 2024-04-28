891cbb018dce68c4ed4063314f9ba1641b581f09
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-04-28 11:25:37 +0600

    skeleton preloader added for opportunity section carousel

---
+++ components/shared/CarouselSkeleton.jsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function CarouselSkeleton() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-14 main-container max-screen-width">
+      <Skeleton className="w-full h-[350px] rounded-xl" />
+      <Skeleton className="w-full h-[350px] rounded-xl hidden md:block" />
+      <Skeleton className="w-full h-[350px] rounded-xl hidden lg:block" />
+    </div>
+  );
+}
